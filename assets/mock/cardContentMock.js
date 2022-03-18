ddf12b00fc84496da2984a97173f1b41544b8598
Maria Infantes Lacal <minfantesl@ext.vectoritcgroup.com>
2022-03-18 12:10:17 +0100

    feat: mock created

---
+++ assets/mock/cardContentMock.js
@@ -0,0 +1,18 @@
+const bandsMock = [
+  "Paramore",
+  "My Chemical Romance",
+  "Blink 182",
+  "SUM 41",
+  "Avril Lavigne",
+  "All Time Low",
+  "Panic! at the disco",
+  "Simple Plan",
+  "Fall Out Boy",
+  "The All-American Rejects",
+  "Yellowcard",
+  "A Day to Remember",
+  "Rise Aganist",
+  "Green Day",
+  "Tonight Alive",
+  "The Offspring"
+];
