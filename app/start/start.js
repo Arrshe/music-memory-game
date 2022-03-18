45b0b21a2691cbaa736f4266e350df0956fb3db2
Maria Infantes Lacal <minfantesl@ext.vectoritcgroup.com>
2022-03-18 12:41:56 +0100

    feat: create card function created

--- app/start/start.js
+++ app/start/start.js
@@ -1,6 +1,7 @@
 import { bandsMock } from "../../assets/mock/cardContentMock";
 
 let cardContentArray = [];
+let cardContainer = document.getElementById("cardContainer");
 
 function fillArray() {
   for (let i = 0; i < 2; i++) {
@@ -13,4 +14,9 @@ function createCardContent() {
   cardContentArray.sort(function () {
     return Math.random() - 0.5;
   });
+}
+
+function createCard() {
+  let card = document.createElement("div");
+  card.appendChild(cardContainer);
 }
